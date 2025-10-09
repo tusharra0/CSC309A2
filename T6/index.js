bc4d3ecf2242399826f04c11a56e9dfd24b8e915
[notadome] <[domenico.nota@mail.utoronto.ca]>
2025-10-09 13:57:20 -0400

    line fix t6

--- T6/index.js
+++ T6/index.js
@@ -21,6 +21,9 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
