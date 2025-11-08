bb35cab7956348bb943772e1e23eb68476dae321
[notadome] <[domenico.nota@mail.utoronto.ca]>
2025-11-07 19:56:43 -0500

    21

--- A2/src/services/userService.js
+++ A2/src/services/userService.js
@@ -31,65 +31,64 @@ const createUser = async ({ utorid, name, email, password, role, verified }) => 
 
 const fetchUsers = async ({ name, role, verified, activated, page = 1, limit = 10 }) => {
   const where = {};
-
-  if (name) {
-    where.OR = [
-      {
-        name: {
-          contains: name,
-          mode: 'insensitive'
+    if (name) {
+      where.OR = [
+        {
+          name: {
+            contains: name,
+            mode: 'insensitive'
+          }
+        },
+        {
+          utorid: {
+            contains: name,
+            mode: 'insensitive'
+          }
         }
-      },
-      {
-        utorid: {
-          contains: name,
-          mode: 'insensitive'
+      ];
+    }
+
+    if (role) {
+      where.role = role;
+    }
+
+    if (typeof verified === 'boolean') {
+      where.verified = verified;
+    }
+
+    if (typeof activated === 'boolean') {
+      where.lastLogin = activated ? { not: null } : null;
+    }
+
+    const skip = (page - 1) * limit;
+
+    const [count, results] = await Promise.all([
+      prisma.user.count({ where }),
+      prisma.user.findMany({
+        where,
+        skip,
+        take: limit,
+        orderBy: { id: 'asc' },
+        select: {
+          id: true,
+          utorid: true,
+          name: true,
+          email: true,
+          birthday: true,
+          role: true,
+          points: true,
+          createdAt: true,
+          lastLogin: true,
+          verified: true,
+          avatarUrl: true
         }
-      }
-    ];
-  }
+      })
+    ]);
 
-  if (role) {
-    where.role = role;
-  }
+    return { count, results };
+  };
 
-  if (typeof verified === 'boolean') {
-    where.verified = verified;
-  }
-
-  if (typeof activated === 'boolean') {
-    where.lastLogin = activated ? { not: null } : null;
-  }
-
-  const skip = (page - 1) * limit;
-
-  const [count, results] = await Promise.all([
-    prisma.user.count({ where }),
-    prisma.user.findMany({
-      where,
-      skip,
-      take: limit,
-      orderBy: { id: 'asc' },
-      select: {
-        id: true,
-        utorid: true,
-        name: true,
-        email: true,
-        birthday: true,
-        role: true,
-        points: true,
-        createdAt: true,
-        lastLogin: true,
-        verified: true,
-        avatarUrl: true
-      }
-    })
-  ]);
-
-  return { count, results };
-};
-
-module.exports = {
-  createUser,
-  fetchUsers
-};
+  module.exports = {
+    createUser,
+    fetchUsers
+  };
